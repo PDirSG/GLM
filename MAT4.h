72fa0de903f6cb8767624977af91b9d3c922dd19
H.İ <PDirectorySG@gmail.com>
2016-09-24 22:37:44 +0300

    Update MAT4.h

--- MAT4.h
+++ MAT4.h
@@ -5,13 +5,7 @@ typedef float MAT4[4][4];
 
 void identityMatrix(MAT4 matrix)
 {
-	for(int X = 0; X < 4; X++)
-	{
-		for(int Y = 0; Y < 4; Y++)
-		{	
-			matrix[X][Y] = 0;
-		}
-	}
+	memset(matrix, 0, sizeof(matrix[0][0]) * 4 * 4);
 	
 	for(int I = 0; I < 4; I++)
 	{
