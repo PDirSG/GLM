ed9b5032af485e6e735c6c7fa249df355102ce3a
H.İ <PDirectorySG@gmail.com>
2016-10-03 22:07:38 +0300

    Update math.c

--- math.c
+++ math.c
@@ -150,13 +150,13 @@ void _ScaleMatrix(float *A, float X, float Y, float Z)
 void _OrthographicMatrix(float *A, float left, float right, float bottom, float top, float near, float far)
 {
 	_MAT4
-		(
-			A,
-			2 / (right - left),                  0,                  0, -((right+left) / (right-left)),
-					 0, 2 / (top - bottom),                  0, -((top+bottom) / (top-bottom)),
-				 	 0,		     0,  -2 / (far - near), -((far + near) / (far - near)),
-					 0,		     0,			 0, 		        	 1
-		);
+	(
+		A,
+		2 / (right - left),                  0,                  0, -((right+left) / (right-left)),
+				 0, 2 / (top - bottom),                  0, -((top+bottom) / (top-bottom)),
+			 	 0,		     0,  -2 / (far - near), -((far + near) / (far - near)),
+				 0,		     0,			 0, 		        	 1
+	);
 }
 
 void _PerspectiveMatrix(float *A/*, ...*/)
